a7852549fab2700e06165d43230c38813775b7ab
vishnu-hue <66674757+vishnu-hue@users.noreply.github.com>
2020-09-09 13:32:31 +0530

    Update decoder_example.py

--- bindings/python/examples/decoder_example.py
+++ bindings/python/examples/decoder_example.py
@@ -17,7 +17,7 @@ from ctcdecoder.decoder import (
     SmearingMode,
     Trie,
 )
-from ctcdecoder import merge_ctc_output
+from ctcdecoder import merge_ctc_outputs
 
 
 # def ptr_as_bytes(x):
@@ -215,7 +215,7 @@ if __name__ == "__main__":
     print("expected output:the fake friend of the famly hae te")
     print(f"Decoding complete, obtained {len(results)} results")
     print("Showing top 5 results:")
-    merged_results,_=merge_ctc_output(results,sil_idx,blank_idx,token_dict,5)
+    merged_results,_=merge_ctc_outputs(results,sil_idx,blank_idx,token_dict,5)
     for i in range(len(merged_results)):
         print(f"score={merged_results[i][0]} prediction='{merged_results[i][1]}'")
     print("expected output:the fake friend of the famly hae taira pt")
